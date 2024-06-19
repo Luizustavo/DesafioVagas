5e8bea8a6dcfefbc8b34b176b2bf3ad120afc840
Luizustavo <126347767+Luizustavo@users.noreply.github.com>
2024-06-19 19:47:31 -0300

    add rounded in input

--- my-project/src/components/atoms/input/index.tsx
+++ my-project/src/components/atoms/input/index.tsx
@@ -11,13 +11,13 @@ interface InputProps {
 
 const Input: React.FC<InputProps> = ({ label, type, value, onChange, className = '' }) => {
   return (
-    <div className={`flex flex-col mb-2  ${className}`}>
+    <div className={`flex flex-col mb-2 ${className}`}>
       <Typography variant="subtitle">{label}</Typography>
       <input
         type={type}
         value={value}
         onChange={onChange}
-        className="border p-2 w-full"
+        className="border p-2 w-full rounded-lg"
       />
     </div>
   );
